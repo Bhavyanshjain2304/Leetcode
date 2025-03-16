9f785ff88cc11152cd1e946a488e8494bdd67b03
Bhavyanshjain2304 <bhavyanshjain432@gmail.com>
2025-03-16 15:54:23 +0530

    Time: 29 ms (77.15%) | Memory: 55.3 MB (81.45%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        long start = Long.MAX_VALUE;
+        long end = Long.MIN_VALUE;
+        for(int rank : ranks){
+            if(rank < start){
+                start = rank;
+            }
+            if(rank>end){
+                end = rank;
+            }
+        }
+        end = end*cars*cars;
+        long ans = 0;
+        while(start <= end){
+            long mid = start + (end-start)/2;
+            if(isCarsRepaired(mid,ranks,cars)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;
+
+    }
+    // n = Math.sqrt((t/r));
+    public boolean isCarsRepaired(long time,int[] ranks, int carsToBeRepaired){
+        long carsRepaired = 0;
+        for(int rank : ranks){
+            carsRepaired += (long) (Math.sqrt((1.0*time) / rank));
+            if(carsRepaired >= carsToBeRepaired) return true;
+        }
+        if(carsRepaired >= carsToBeRepaired) return true;
+        else return false;
+    }
+}
+
+
