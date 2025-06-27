c390c8488bb7e52b255f5a6e832445d7ad51c53b
Bhavyanshjain2304 <bhavyanshjain432@gmail.com>
2025-06-27 12:13:25 +0530

    Time: 4 ms (100.00%) | Memory: 44.5 MB (18.23%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x<0){
+            return false;
+        }else if(x==0){
+            return true;
+        }
+        int org = x;
+        long ans = 0;
+        while(x>0){
+            int rem = x%10;
+            ans = ans*10 + rem;
+            x = x/10;
+        }
+        
+        return ans==org;
+
+        
+    }
+}
