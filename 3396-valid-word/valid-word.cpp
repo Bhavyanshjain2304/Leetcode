da99e411c35c7135569785c660b350716785f478
Bhavyanshjain2304 <bhavyanshjain432@gmail.com>
2025-07-15 13:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.48%) - LeetSync

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if(word.length()<3) return false;
+        int v = 0,c = 0;
+        for(auto i : word){
+            if(isalpha(i)){
+                if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'){
+                    v++;
+                }else if(i=='A'||i=='E'||i=='I'||i=='O'||i=='U'){
+                    v++;
+                }else{
+                    c++;
+                }
+            }
+            if(! (isalpha(i) || isdigit(i))){
+                return false;
+            }
+        }
+        if(v>=1&&c>=1) return true;
+        else return false;
+
+    }
+};
